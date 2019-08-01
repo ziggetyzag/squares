fec3193d5c1a416882bf3603747cbf6034635326
ziggetyzag <jrodolfovillasenor@hotmail.com>
2019-07-31 21:38:43 -0500

    Add files via upload

---
+++ precache-manifest.fc4d6afccbf86d4e840e742b234e5f50.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc9f789726fd63b97396f911eaa552e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb76a4880d6e64fb05a0",
+    "url": "/static/css/2.b7bf3e08.chunk.css"
+  },
+  {
+    "revision": "2e77d1a479e5c3db9698",
+    "url": "/static/css/main.3aa4a370.chunk.css"
+  },
+  {
+    "revision": "fb76a4880d6e64fb05a0",
+    "url": "/static/js/2.bf55a04f.chunk.js"
+  },
+  {
+    "revision": "2e77d1a479e5c3db9698",
+    "url": "/static/js/main.e804281f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
